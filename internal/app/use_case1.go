035b8aaeecde1972637d9c1a2f4d1969777bc85f
Mazen Kenjrawi <mazen270@hotmail.com>
2022-08-29 22:38:18 +0200

    this is just a draft branch and a PoC for me to start improving the code design later

--- internal/app/use_case1.go
+++ internal/app/use_case1.go
@@ -1,9 +1,16 @@
 package app
 
 import (
+	"concurrent-bus/internal/domain"
 	"fmt"
 	"time"
 )
+
+var counter int
+
+func init() {
+	counter = 3
+}
 
 type Cmd1 struct {
 	// ... fields
@@ -13,12 +20,25 @@ type Hndlr1 struct {
 	// ... dependencies
 }
 
-func (h *Hndlr1) Handle(cmd Cmd1) error {
-	// memic the handling period using sleep
-	time.Sleep(time.Second)
-
+func (h *Hndlr1) Handle(cmd Cmd1, domainBus chan<- interface{}) error {
+	counter--
 	now := time.Now().Format(time.StampMilli)
 	fmt.Println(now, "Use-Case 1, handling command normally...")
+
+	fmt.Println("counter is ", counter)
+
+	if counter > 0 {
+		mutatedAgg1, err := domain.MutateAggregate1("foo", 5, domainBus)
+		if err != nil {
+			return err
+		}
+
+		// do something with the aggregate
+		// memic the handling period using sleep
+		fmt.Println(mutatedAgg1)
+	}
+
+	time.Sleep(time.Second)
 
 	return nil
 }
--- internal/infra/bus.go
+++ internal/infra/bus.go
@@ -3,26 +3,86 @@ package infra
 import (
 	"fmt"
 	"reflect"
+	"sync"
 
 	"concurrent-bus/internal/app"
 )
 
-type Bus struct {
+type bus struct {
+	// domain bus to transmit domain events
+	domainBus chan interface{}
+	errChan   chan error
+
+	eventCmds []interface{}
+
 	hndlr1 app.Hndlr1
 	hndlr2 app.Hndlr2
 }
 
-// typical approach
-func (b *Bus) Handle(cmd interface{}) error {
+func NewBus() bus {
+	domainBus := make(chan interface{})
+	errChan := make(chan error)
+
+	return bus{
+		domainBus,
+		errChan,
+		[]interface{}{},
+		app.Hndlr1{},
+		app.Hndlr2{},
+	}
+}
+
+func (b *bus) Handle(cmd interface{}) error {
+	go b.handle(cmd)
+
+	for {
+		select {
+		case err := <-b.errChan:
+			if err != nil {
+				return err
+			}
+			return b.handleDomainEvents()
+		case domainEvent := <-b.domainBus:
+			b.registerEventAsCommand(domainEvent)
+		}
+	}
+}
+
+func (b *bus) handleDomainEvents() error {
+	var wg sync.WaitGroup
+	wg.Add(len(b.eventCmds))
+	for _, cmd := range b.eventCmds {
+		go func(c interface{}) {
+			defer wg.Done()
+			b.Handle(c)
+		}(cmd)
+	}
+	wg.Wait()
+	return nil
+}
+
+func (b *bus) registerEventAsCommand(domainEvent interface{}) {
+	eventType := getType(domainEvent)
+	fmt.Println("registering event with type ", eventType)
+
+	b.eventCmds = append(b.eventCmds, app.Cmd2{})
+	b.eventCmds = append(b.eventCmds, app.Cmd2{})
+	b.eventCmds = append(b.eventCmds, app.Cmd2{})
+	// b.eventCmds = append(b.eventCmds, app.Cmd1{})
+}
+
+func (b *bus) handle(cmd interface{}) {
 	cmdType := getType(cmd)
 	switch cmdType {
 	case "Cmd1":
-		return b.hndlr1.Handle(cmd.(app.Cmd1))
+		b.errChan <- b.hndlr1.Handle(cmd.(app.Cmd1), b.domainBus)
+		return
 	case "Cmd2":
-		return b.hndlr2.Handle(cmd.(app.Cmd2))
+		b.errChan <- b.hndlr2.Handle(cmd.(app.Cmd2))
+		return
 	}
 
-	return fmt.Errorf("unable to find handler for cmd %s", cmdType)
+	b.errChan <- fmt.Errorf("unable to find handler for cmd %s", cmdType)
 }
 
 func getType(v interface{}) string {
